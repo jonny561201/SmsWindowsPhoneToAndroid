377b59679687d28ba37b7ae89b50327667dfc0e4
Jon Graf <jongraf1@gmail.com>
2017-03-26 13:26:38 -0500

    Added initial test file and updated git ignore to ignore assemblyinfo files

---
+++ WindowsPhoneToAndroidSMSBackup/WinowsPhoneToAndroid.Tests/ExtractWindowsPhoneTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace WindowsPhoneToAndroidSMSBackup.WinowsPhoneToAndroid.Tests
+{
+    [TestFixture]
+    public class ExtractWindowsPhoneTests
+    {
+        public string Message = "<Message><Recepients/>< Body > Ooh good to know!! Thanks</Body><IsIncoming>true</IsIncoming><IsRead>true</IsRead><Attachments/><LocalTimestamp>131348483095578379</LocalTimestamp><Sender>5153138947</Sender></Message>";
+        public ExtractWindowsPhone extractWindows;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+    }
+}
+
+//<Message><Recepients/>
+//    <Body>Ooh good to know!! Thanks
+//    </Body>
+//    <IsIncoming>true</IsIncoming>
+//    <IsRead>true</IsRead><Attachments/>
+//    <LocalTimestamp>131348483095578379</LocalTimestamp>
+//    <Sender>5153138947</Sender>
+//</Message>
