f1f589ff4efbd4e28fa26c23bfb133fe5713ab52
Jon Graf <jongraf1@gmail.com>
2017-03-29 22:56:37 -0500

    Added main method with test runner to execute the project

---
+++ WindowsPhoneToAndroidSMSBackup/WindowsPhoneToAndroid/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+
+namespace WindowsPhoneToAndroidSMSBackup.WindowsPhoneToAndroid
+{
+    public class Main
+    {
+        public void Execute()
+        {
+            var extractor = new ExtractWindowsPhone();
+            var converter = new ConvertToAndroid();
+            var controller = new TransformController(extractor, converter);
+            string windowsPhoneXml;
+            using (var streamReader = new StreamReader("C:\\Users\\Inuyasha\\Desktop\\smsBackup\\Messages_Backup.xml", Encoding.UTF8))
+            {
+                windowsPhoneXml = streamReader.ReadToEnd();
+            }
+
+            var xmlDoc = controller.Transform(windowsPhoneXml);
+            xmlDoc.Save("C:\\Users\\Inuyasha\\Desktop\\smsBackup\\Messages_Backup_new.xml");
+        }
+    }
+}
---
+++ WindowsPhoneToAndroidSMSBackup/WinowsPhoneToAndroid.Tests/MainTest.cs
@@ -0,0 +1,17 @@
+﻿using WindowsPhoneToAndroidSMSBackup.WindowsPhoneToAndroid;
+using NUnit.Framework;
+
+namespace WindowsPhoneToAndroidSMSBackup.WinowsPhoneToAndroid.Tests
+{
+    [TestFixture]
+    public class MainTest
+    {
+        [Test]
+        public void ExecuteMethod()
+        {
+            var execute = new Main();
+
+            execute.Execute();
+        }
+    }
+}
